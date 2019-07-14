0a2df0720e34d0b3cacf6d16772dee7474234984
Eyal Shenitzky <eshenitz@redhat.com>
2019-07-14 13:27:58 +0300

    fail_over: use input() instead of raw_input()
    raw_input() was renamed to input() in python3
    
    According to pylint report -
    Undefined variable 'raw_input' (undefined-variable)
    
    Change-Id: I72c1f84618504ce6ce9a20f4e83f7ad8bb90549c
    Signed-off-by: Eyal Shenitzky <eshenitz@redhat.com>

--- files/fail_over.py
+++ files/fail_over.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+from six.moves import input
+
 from bcolors import bcolors
 from ConfigParser import SafeConfigParser
 import logging
@@ -58,7 +60,7 @@ class FailOver():
         cmd.append("--vault-password-file")
         cmd.append("vault_secret.sh")
         cmd.append("-vvv")
-        vault_pass = raw_input(
+        vault_pass = input(
             INPUT + PREFIX + "Please enter the vault password: " + END)
         os.system("export vault_password=\"" + vault_pass + "\"")
         log.info("Executing failover command: %s" % ' '.join(map(str, cmd)))
@@ -160,40 +162,31 @@ class FailOver():
                                                        _SECTION,
                                                        ansible_play=None))
         while target_host not in setups:
-            target_host = raw_input(
+            target_host = input(
                 INPUT + PREFIX + "target host was not defined. "
                 "Please provide the target host "
                 "(primary or secondary): " + END)
         while source_map not in setups:
-            source_map = raw_input(
+            source_map = input(
                 INPUT + PREFIX + "source mapping was not defined. "
                 "Please provide the source mapping "
                 "(primary or secondary): " + END)
         while not os.path.isfile(var_file):
-            var_file = raw_input("%s%svar file mapping '%s' does not exist. "
-                                 "Please provide a valid mapping var file: %s"
-                                 % (INPUT,
-                                    PREFIX,
-                                    var_file,
-                                    END))
+            var_file = input("%s%svar file mapping '%s' does not exist. "
+                             "Please provide a valid mapping var file: %s"
+                             % (INPUT, PREFIX, var_file, END))
         while not os.path.isfile(vault):
-            vault = raw_input("%s%spassword file '%s' does not exist. "
-                              "Please provide a valid password file:%s "
-                              % (INPUT,
-                                 PREFIX,
-                                 vault,
-                                 END))
+            vault = input("%s%spassword file '%s' does not exist. "
+                          "Please provide a valid password file:%s "
+                          % (INPUT, PREFIX, vault, END))
         while (not ansible_play) or (not os.path.isfile(ansible_play)):
-            ansible_play = raw_input("%s%sansible play '%s' "
-                                     "is not initialized. "
-                                     "Please provide the ansible play file "
-                                     "to generate the mapping var file "
-                                     "with ('%s'):%s "
-                                     % (INPUT,
-                                        PREFIX,
-                                        str(ansible_play),
-                                        PLAY_DEF,
-                                        END) or PLAY_DEF)
+            ansible_play = input("%s%sansible play '%s' "
+                                 "is not initialized. "
+                                 "Please provide the ansible play file "
+                                 "to generate the mapping var file "
+                                 "with ('%s'):%s "
+                                 % (INPUT, PREFIX, str(ansible_play),
+                                    PLAY_DEF, END) or PLAY_DEF)
         return (target_host, source_map, var_file, vault, ansible_play)
 
     def _set_log(self, log_file, log_level):

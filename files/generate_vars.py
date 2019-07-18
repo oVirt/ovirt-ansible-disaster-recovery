99591d9dc415dabd0c00996ada5f108c8022d35f
Eyal Shenitzky <eshenitz@redhat.com>
2019-07-18 09:41:52 +0300

    generate_vars: import configparser if ConfigParser not found
    ConfigParser was renamed to configparser in python 3.
    This patch import it in case ConfigParser module not found.
    
    Change-Id: I13bada79f6eaa4323ab33d1cbadf1f3fb90ba202
    Signed-off-by: Eyal Shenitzky <eshenitz@redhat.com>

--- files/generate_vars.py
+++ files/generate_vars.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
-from ConfigParser import SafeConfigParser
+try:
+    from ConfigParser import SafeConfigParser
+except ModuleNotFoundError:
+    from configparser import SafeConfigParser
 
 import logging
 import os.path
